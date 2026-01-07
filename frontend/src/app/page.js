c09fc031aea3890cef364a9f0b313c19e194eb47
romman-khan <rommankhan05@gmail.com>
2026-01-07 11:10:11 +0530

    resolve api error

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}reviews`,
+        `${process.env.NEXT_PUBLIC_API_URL}/reviews/`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
